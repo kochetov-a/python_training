b1b917e9f8be8f795e9c6aed18953cf4ee2da1fe
kochetov-a <kochetov.a.s@gmail.com>
2016-11-06 20:30:25 +0300

    Revert "Выделены классы для работы с контактами"
    This reverts commit 29b879a932d61797d68d8837291e541019aab248.

--- contacttest/add_contact.py
+++ contacttest/add_contact.py
@@ -5,6 +5,7 @@ import pytest
 from contactfixture.application_c import Application_c
 from contactmodel.contact import Contact
 
+
 @pytest.fixture
 def app(request):
     fixture = Application_c()
@@ -12,11 +13,11 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(first_name="Ivan", second_name="Ivanovich", last_name="Ivanov"))
-    app.session.logout()
+    app.login(username="admin", password="secret")
+    app.add_new_contact(Contact(first_name="Ivan", second_name="Ivanovich", last_name="Ivanov"))
+    app.logout()
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(first_name="", second_name="", last_name=""))
-    app.session.logout()
+    app.login(username="admin", password="secret")
+    app.add_new_contact(Contact(first_name="", second_name="", last_name=""))
+    app.logout()
