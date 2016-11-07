1c6b800793982cac5940d13fc23b0ba2a65518e2
kochetov-a <kochetov.a.s@gmail.com>
2016-11-07 23:52:00 +0300

    Сохранение рабочего состояния

--- fixture/contact.py
+++ fixture/contact.py
@@ -23,3 +23,11 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         # return_home_page
         wd.find_element_by_link_text("home").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # delete first contact
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
