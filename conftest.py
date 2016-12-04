6aebfc64e4de5d84d33f5fe8d3ad7c5ddf20bf11
kochetov-a <kochetov.a.s@gmail.com>
2016-12-04 16:08:14 +0300

    Конфигурационный файл для тестов

--- conftest.py
+++ conftest.py
@@ -2,28 +2,28 @@
 
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 fixture = None
+target = None
 
 # Фикстура логина на сайт с проверкой валидности фикстуры
 @pytest.fixture
 def app(request):
-    global fixture
-    browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    login = request.config.getoption("--login")
-    password = request.config.getoption("--password")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        # Если фикстура не валидна
-        if not fixture.is_valid():
-            # Инициализируем фикстуру
-            fixture = Application(browser=browser, base_url=base_url)
+    global fixture  # Объявление глобальной переменной для фикстуры
+    global target  # Объявление глобальной переменной для файла конфигурации
+    if target is None:
+        # Получаем месторасположение конфига из переменной __file__ и объеденяем его с "target.json"
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:  # Пробуем открыть файл конфигурации и присвоить его переменной "f"
+            target = json.load(f)  # Загружаем в переменную "target" содержимое файла
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=target["browser"], base_url=target["baseUrl"])
     # Открываем главную страницу в любом случае
     fixture.open_home_page()
     # Выполняем логин в любом случае
-    fixture.session.ensure_login(username=login, password=password)
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 # Фикстура выхода из приложения
@@ -38,7 +38,4 @@ def stop(request):
     return fixture
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--login", action="store", default="Admin")
-    parser.addoption("--password", action="store", default="secret")
+    parser.addoption("--target", action="store", default="target.json")
